5511970d3f60bffb6dd586a996b1a7737612b88e
Juan Pablo Jofre <ju.jofrec@duocuc.cl>
2026-05-18 20:59:03 -0400

    Inicio de proyecto

---
+++ FacturacionM/src/main/java/com/Facturacion/FacturacionM/FacturacionMApplication.java
@@ -0,0 +1,13 @@
+package com.Facturacion.FacturacionM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionMApplication.class, args);
+	}
+
+}
---
+++ FacturacionM/src/test/java/com/Facturacion/FacturacionM/FacturacionMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Facturacion.FacturacionM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
